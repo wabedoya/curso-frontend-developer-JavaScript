10e33f982b0648ca628100897e1a56e8e3234f49
Wilson Alexis Bedoya <wilson.bedoya24@gmail.com>
2023-03-14 10:39:06 -0500

    agregando carrito de compra al archivo index.html, style.css y main.js

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const menuEmail= document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const MenuHamIcon = document.querySelector('.menu');
+const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-menuEmail.addEventListener('click',toggleDesktopMenu);
-MenuHamIcon.addEventListener('click',toggleMobileMenu);
 
+const aside = document.querySelector('.product-detail');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside)
 
 function toggleDesktopMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 
 }
+
+
 function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+    
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+function toggleCarritoAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopClose = desktopMenu.classList.contains('inactive');
+    
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isDesktopClose){
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
+    
 }
